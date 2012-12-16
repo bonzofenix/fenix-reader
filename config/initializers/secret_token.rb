65b1412fb4c299992af8b8b634e5d26add580665
bonzofenix <bonzofenix@gmail.com>
2012-12-16 10:55:14 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FenixReader::Application.config.secret_token = 'c695e0f78e9a07e9dabd36c36075218dced818754fb9506e1607aaf7d4a08ba0d9f719f265eff3d1d0cfb118440b414bca379c904ac9d18d6c11fa6ddec6604c'
