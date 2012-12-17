0761723281090e50f23cc7eea101d72aa58655fe
bonzofenix <bonzofenix@gmail.com>
2012-12-17 02:18:16 -0300

    letter opener configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ FenixReader::Application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
@@ -32,6 +31,9 @@ FenixReader::Application.configure do
   }
 
 
+
+  # Configuration for letter opener
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
